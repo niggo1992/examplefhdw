c636d31180c5bd30629fe3bb8138b9995739e512
nico <ngoronz@gmail.com>
2017-03-22 21:09:54 +0100

    index.js fixed

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 /**
  * Created by nico_ on 22.03.2017.
  */
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+    res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
