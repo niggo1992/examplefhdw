92141d4dda41f734211bc4ce55f4a744b03bf06c
nico <ngoronz@gmail.com>
2017-03-26 15:30:24 +0200

    index.html hinzugefügt

--- index.js
+++ index.js
@@ -4,14 +4,13 @@
 'use strict';
 
 const express = require('express');
-
-// Constants
-const PORT = 8000;
-
-// App
+const PORT = 8080;
 const app = express();
+var path = require('path');
+
 app.get('/', function (req, res) {
     res.send('Hello world\n');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT);
